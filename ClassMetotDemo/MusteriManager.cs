f20e086b28be0b0f0d573b4c03f0f5b28e29cbb3
Buse <busebudak47@gmail.com>
2021-03-04 20:14:55 +0300

    3.ders ödevi

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri eklendi : " + musteri.Adi + " " + musteri.Soyadi);
+        }
+
+        public void Listele(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri listelendi : " + musteri.Adi + " " + musteri.Soyadi );
+        }
+
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri silindi : " + musteri.Adi + " " + musteri.Soyadi);
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.Adi = "Berfin";
+            musteri1.Soyadi = "BUDAK";
+            musteri1.Yasi = 29;
+            musteri1.YasadigiYer = "İstanbul/KÜÇÜKÇEKMECE";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.Adi = "Tülay";
+            musteri2.Soyadi = "BALCIOĞLU";
+            musteri2.Yasi = 60;
+            musteri2.YasadigiYer = "İstanbul/KÜÇÜKÇEKMECE";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 3;
+            musteri3.Adi = "Ayşe";
+
+
+
+            musteri3.Soyadi = "ERÇİN";
+            musteri3.Yasi = 23;
+            musteri3.YasadigiYer = "İstanbul/BEYLİKDÜZÜ";
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Id);
+                Console.WriteLine(musteri.Adi);
+                Console.WriteLine(musteri.Soyadi);
+                Console.WriteLine(musteri.Yasi);
+                Console.WriteLine(musteri.YasadigiYer);
+            }
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+            musteriManager.Ekle(musteri3);
+
+            musteriManager.Listele(musteri1);
+            musteriManager.Listele(musteri2);
+            musteriManager.Listele(musteri3);
+
+            musteriManager.Sil(musteri3);
+            
+        }
+    }
+}
